09234b6f58a7ad6415b595a17bf711b484940cde
Vinit Parikh <vinitp94@gmail.com>
2018-07-05 16:42:46 -0700

    Add account link logos

--- src/components/building-blocks/footer.js
+++ src/components/building-blocks/footer.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React   from 'react';
+import Utility from '../../utility';
+
 
 class Footer extends React.Component {
   constructor(props) {
@@ -22,7 +24,7 @@ class Footer extends React.Component {
   }
 
   submitForm() {
-    window.location.href = 'mailto:contact@mach4.app?subject=' + this.state.subject + '&body=From: ' + this.state.email + '%0A%0A' + encodeURIComponent(this.state.message);
+    window.location.href = 'mailto:vinitp94@gmail.com?subject=' + this.state.subject + '&body=From: ' + this.state.email + '%0A%0A' + encodeURIComponent(this.state.message);
   }
 
   render() {
@@ -30,6 +32,7 @@ class Footer extends React.Component {
       <div className='oe-footer'>
         <div className='footer-container'>
           <div className='footer-title'>BOOKINGS</div>
+          <div className='booking-description'>{ 'Thank you for considering Oasis Events for your next event! Please fill out the form below to give us a sense of what you\'re looking for and we\'ll get back to you within 24-48 hours. If you\'re in a rush, not to worry; feel free to call us at (408) 838 7249 instead!' }</div>
 
           <div className={'form-field subject'} onFocus={ this.focusInput.bind(this, 'subject') }>
             <input className='input-elem' onChange={ this.handleUserInput.bind(this, 'subject') }/>
@@ -54,7 +57,9 @@ class Footer extends React.Component {
             <div className='developer-credits'>Developed by <a href='http://mach4.app/' target='_blank'>MACH4 Technologies Inc.</a></div>
 
             <div className='account-links'>
-
+              <a href='' target='_blank'><img className='account-link-item' src={ Utility.buildAssetUrl('logos/instagram.svg') }/></a>
+              <a href='' target='_blank'><img className='account-link-item' src={ Utility.buildAssetUrl('logos/facebook.svg') }/></a>
+              <a href='' target='_blank'><img className='account-link-item' src={ Utility.buildAssetUrl('logos/yelp.svg') }/></a>
             </div>
           </div>
         </div>
