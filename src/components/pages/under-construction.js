99fd74323cb278752b8b8a084839afed647698e7
Vinit Parikh <vinitp94@gmail.com>
2019-01-26 16:38:45 -0800

    Update email text

--- src/components/pages/under-construction.js
+++ src/components/pages/under-construction.js
@@ -5,7 +5,7 @@ export default function UnderConstruction() {
   return (
     <div className='oe-under-construction'>
       <img className='logo' src={Utility.buildAssetUrl('logos/oe.png')}/>
-      <div className='message'>The website is currently going through scheduled maintenance! For bookings, contact <a className='link-text' href='mailto:vinitp94@gmail.com?subject='>www.oasisevents.us</a></div>
+      <div className='message'>The website is currently going through scheduled maintenance! For bookings, contact <a className='link-text' href='mailto:vinitp94@gmail.com?subject='>contact@oasisevents.us</a></div>
     </div>
   );
 }
